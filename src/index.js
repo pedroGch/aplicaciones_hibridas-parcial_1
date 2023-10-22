d094b7b4dc7137b7c441573081ba5db282b8b3e2
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-22 20:39:45 -0300

    mejoro puerto

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import express from 'express'
 import proyectosRoutes from './routes/proyectos.routes.js'
 
 const app = express()
+const port = 2222
 
 app.use(express.json())
 
 app.use(proyectosRoutes)
 
-app.listen(2222, () => {
-  console.log(`estas conectado al puerto 2222`)
+app.listen(port, () => {
+  console.log(`estas conectado al puerto: ${port}`)
 })
