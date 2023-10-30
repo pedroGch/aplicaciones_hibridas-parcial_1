b6a6379bc7d0ad686a23d23e5a48eef41264ee36
pedro <pedro.gch01@gmail.com>
2023-10-30 14:59:01 -0300

    agrego el voto services a jueces services

--- src/services/jueces.services.js
+++ src/services/jueces.services.js
@@ -1,4 +1,9 @@
 import { MongoClient, ObjectId } from 'mongodb'
+import votosServices from "./votos.services.js"
+
+const cliente = new MongoClient('mongodb://127.0.0.1:27017')
+const db = cliente.db("AH_PARCIAL1")
+const juegosCollection = db.collection("judges")
 
 /**
  * Obtiene mediante el id de un juez, los juegos que fueron que fueron votados 
@@ -18,10 +23,7 @@ async function juegosVotados(id) {
  * @returns {} 
  */
 async function emitirVoto(data){
-  return {
-    '_id': new ObjectId(),
-    'nombre': 'Final Fantasy 9'
-  }
+  return votosServices.almacenarVoto(data)
 }
 
 
