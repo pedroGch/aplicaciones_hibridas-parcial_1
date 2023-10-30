e03c43c9a9e673df902071c7338e93b83d6403e9
pedro <pedro.gch01@gmail.com>
2023-10-30 14:59:29 -0300

    ok

--- src/services/juegos.services.js
+++ src/services/juegos.services.js
@@ -1,12 +1,18 @@
 import { MongoClient, ObjectId } from 'mongodb'
 
+const cliente = new MongoClient('mongodb://127.0.0.1:27017')
+const db = cliente.db("AH_PARCIAL1")
+const juegosCollection = db.collection("games")
+
 async function jucesQueVotaronA (id_juego) {
+  await cliente.connect()
   return{
     '_id': new ObjectId(),
     'nombre_juez' : 'patricio' 
   }
 }
 async function obtenerPorEdidicion (edicion){
+  await cliente.connect()
   return{
     '_id': new ObjectId(),
     'nombre_juez' : 'patricio', 
