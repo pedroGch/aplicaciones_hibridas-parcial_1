29208331d29f2b5e529a5050fca3c5f8899d5398
pedro <pedro.gch01@gmail.com>
2023-11-02 17:59:24 -0300

    validacion voto agregado

--- src/routes/jueces.routes.js
+++ src/routes/jueces.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import juecesControllers from '../controllers/jueces.controllers.js'
+import  { validarVotoCreacion }  from '../middlewares/voto.middlewares.js'
 const routeJueces = express()
 
 routeJueces.get('/:id/votosEmitidos', juecesControllers.juegosVotados)
 
-routeJueces.post('/votar', juecesControllers.emitirVoto)
+routeJueces.post('/votar', [validarVotoCreacion], juecesControllers.emitirVoto)
 
 export default routeJueces
