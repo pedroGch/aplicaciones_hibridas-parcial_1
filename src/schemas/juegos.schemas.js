5ca9edff7d64b730627beef3887958e50e96f074
pedro <pedro.gch01@gmail.com>
2023-11-02 17:58:18 -0300

    juego schema terminado

--- src/schemas/juegos.schemas.js
+++ src/schemas/juegos.schemas.js
@@ -1,10 +1,20 @@
 
 import yup from 'yup'
 
+const msj_name_requided    = 'el nombre es obligatorio'
+
+const msj_genre_requided   = 'el genero es obligatorio'
+
+const msj_edition_requided = 'la edición es obligatoria'
+const msj_edition_positive = 'la edicion ingresada no puede ser negativa'
+const msj_edition_integer  = 'la edicion ingresada debe ser un número entero'
+
 const juegoCrearSchema = yup.object({
-  name: yup.string().required(),
-  genre: yup.string().required(),
-  edition : yup.number().positive().integer().required(),
+
+  name    : yup.string().required(msj_name_requided),
+  genre   : yup.string().required(msj_genre_requided),
+  edition : yup.number().positive(msj_edition_positive).integer(msj_edition_integer).required(msj_edition_requided),
+
 })
 
 export default {
