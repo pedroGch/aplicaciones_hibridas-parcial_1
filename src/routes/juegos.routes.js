5feb4d2c21c0297abf53cd429fe5450b22a6affd
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-11-27 22:51:42 -0300

    nuevas rutas

--- src/routes/juegos.routes.js
+++ src/routes/juegos.routes.js
@@ -3,17 +3,17 @@ import  { validarJuegoCreacion }  from '../middlewares/juego.middlewares.js'
 import juegosControllers from '../controllers/juegos.controllers.js'
 const routeJuegos = express()
 
-routeJuegos.get('/:idJuego/info', juegosControllers.juegosPorId)
+routeJuegos.post('/',[validarJuegoCreacion], juegosControllers.crearJuego)
+
+routeJuegos.put('/:id', juegosControllers.editarJuego)
+
+routeJuegos.delete('/:id', juegosControllers.borrarJuego)
+
+routeJuegos.get('/:idJuego', juegosControllers.juegosPorId)
 
 routeJuegos.get('/:idJuego/promedio', juegosControllers.promedio)
 
 routeJuegos.get('/:edicion/edicion', juegosControllers.obtenerPorEdicion)
-
-routeJuegos.post('/crear',[validarJuegoCreacion], juegosControllers.crearJuego)
-
-routeJuegos.put('/:id/editar', juegosControllers.editarJuego)
-
-routeJuegos.delete('/:id/borrar', juegosControllers.borrarJuego)
 
 
 export default routeJuegos
