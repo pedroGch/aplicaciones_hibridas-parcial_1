8cd8dedb34fe713538ebda23e4ff3c9308aca995
pedro <pedro.gch01@gmail.com>
2023-11-30 13:04:04 -0300

    cambio y comento la uri

--- src/routes/jueces.routes.js
+++ src/routes/jueces.routes.js
@@ -3,9 +3,11 @@ import juecesControllers from '../controllers/jueces.controllers.js'
 import  { validarVotoCreacion, juezExiste, juegoExiste, votoUnico }  from '../middlewares/voto.middlewares.js'
 const routeJueces = express()
 
+//trigo todos los votos emitidos por un juez
 routeJueces.get('/:id/votosEmitidos', juecesControllers.juegosVotados)
 
-//routeJueces.post('/votar', [validarVotoCreacion], [juezExiste], [juegoExiste], [votoUnico], juecesControllers.emitirVoto)
-routeJueces.post('/votar', juecesControllers.emitirVoto)
+//creo un voto nuevo
+routeJueces.post('/voto', [validarVotoCreacion], [juezExiste], [juegoExiste], [votoUnico], juecesControllers.emitirVoto)
+
 
 export default routeJueces
